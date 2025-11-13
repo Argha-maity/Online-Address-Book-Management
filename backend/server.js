797ee575222cf93ecb7c6e008cadaa0a9f45c27c
Argha Maity <arghamaity@Arghas-MacBook-Air.local>
2025-11-13 11:11:30 +0530

    server.js changes

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ app.use(cors({
   credentials: true
 }));
 app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
